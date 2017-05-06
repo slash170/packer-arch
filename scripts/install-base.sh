4342fa7c21e80c97ece52ed9151fdb84bec3d252
Yutaka Konishi <slash170@gmail.com>
2017-05-06 22:18:18 +0900

    json / shell: customized for japanese environment

--- scripts/install-base.sh
+++ scripts/install-base.sh
@@ -10,10 +10,10 @@ else
 fi
 
 FQDN='vagrant-arch.vagrantup.com'
-KEYMAP='us'
-LANGUAGE='en_US.UTF-8'
+KEYMAP='jp106'
+LANGUAGE='ja_JP.UTF-8'
 PASSWORD=$(/usr/bin/openssl passwd -crypt 'vagrant')
-TIMEZONE='UTC'
+TIMEZONE='Asia/Tokyo'
 
 CONFIG_SCRIPT='/usr/local/bin/arch-config.sh'
 ROOT_PARTITION="${DISK}1"
